1945b4f4be2d09e921fc202e73a266e12c9b3c00
Solomon <sholasolomon@yahoo.com>
2022-10-11 23:36:44 +0100

    Update settings.py

--- HRAgency/settings.py
+++ HRAgency/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#nwdtkwd9-%jjlpzzfe#f5pu!ti!vq4t2i(ao@gl1be75mp^71'
+#SECRET_KEY = 'django-insecure-#nwdtkwd9-%jjlpzzfe#f5pu!ti!vq4t2i(ao@gl1be75mp^71'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
